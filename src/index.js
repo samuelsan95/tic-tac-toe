e6c85800b5fbb3e82fbb9696d76b30953bcac8e1
Samuel <samuel.sanchez@uxsmobile.com>
2018-09-11 10:22:12 +0200

    add title

--- src/index.js
+++ src/index.js
@@ -136,6 +136,9 @@ class Game extends React.Component {
 
         return (
             <div className="game">
+            <div class="title-game">
+                <h1> TIC TAC TOE </h1>
+            </div>
             <div className="game-board">
                 <Board 
                     squares={current.squares}
